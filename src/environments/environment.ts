447f67809366c54a97a3f79797a8b241cae5f731
AndresDav7 <andres.alvear01@epn.edu.ec>
2020-09-18 18:47:05 -0500

    Ionic Movies API

---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+ firebaseConfig: {
+  apiKey: "AIzaSyBbceEU0M1x85W2pPKz_MQInWpEE3AZznk",
+  authDomain: "ionic-movies-34ba8.firebaseapp.com",
+  databaseURL: "https://ionic-movies-34ba8.firebaseio.com",
+  projectId: "ionic-movies-34ba8",
+  storageBucket: "ionic-movies-34ba8.appspot.com",
+  messagingSenderId: "164647499398",
+  appId: "1:164647499398:web:89867372b91d2545044307",
+  measurementId: "G-3PG5ZDK5XV"
+  },
+};
+
+//Credenciales de Firebase
+//export const firebaseConfig = { }
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
